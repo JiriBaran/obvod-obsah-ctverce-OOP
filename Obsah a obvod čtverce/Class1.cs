f37655bbfe0888f0aa196a97f2405ecd69347465
Jiří Baran <jurka.baran@seznam.cz>
2020-03-30 17:52:15 +0200

    Přidejte soubory projektu.

---
+++ Obsah a obvod čtverce/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsah_a_obvod_ctverce
+{
+    class Ctverec
+    {
+        private double strana;
+        public Ctverec(double strana)
+        {
+            this.strana = strana;
+        }
+
+        public double Obvod()
+        {
+            return strana * 4;
+        }
+
+        public double Obsah()
+        {
+            return strana * strana;
+        }
+    }
+}
---
+++ Obsah a obvod čtverce/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsah_a_obvod_ctverce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ctverec ctverec = new Ctverec(5);
+            Console.WriteLine(ctverec.Obvod());
+            Console.WriteLine(ctverec.Obsah());
+
+            Console.ReadKey();
+        }
+    }
+}
